fafcdcd9bbdbedc821c5079d1a2c6f9ea3a5d8de
intae92 <kangit2013@gmail.com>
2020-07-20 22:07:42 +0900

    express 서버 구축

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const handleListening = () =>
+  console.log(`✅Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
