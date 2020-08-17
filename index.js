021b1b3facb396ef0f135637210c56ccd3ffea78
intae92 <kangit2013@gmail.com>
2020-08-17 23:36:16 +0900

    바벨 설치

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require("express");
+import "core-js";
+import express from "express";
 const app = express();
 const PORT = 4000;
 const handleListening = () =>
   console.log(`✅Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from my ass");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
